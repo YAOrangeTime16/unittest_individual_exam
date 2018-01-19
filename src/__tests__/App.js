a64f51db6a74d25a6a616d715acabeb1074b662c
YAOrangeTime16 <yokoandrae@gmail.com>
2018-01-19 15:16:21 +0100

    added the first test and connected to Travis ci

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
 import App from '../components/App';
 
 test('renders the app', () => {
   render(<App />);
 });
+
+describe('Button', ()=>{
+  const wrapper = mount(<App />);
+  afterEach(()=>{
+    wrapper.unmount();
+  })
+  it('should change state of currentPage', ()=>{
+    expect(wrapper.state().currentPage).toBe('home');
+    wrapper.find('button').simulate('click');
+    expect(wrapper.state().currentPage).toBe('bot');
+
+  });
+})
