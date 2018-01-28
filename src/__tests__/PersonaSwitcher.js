0c837cfefb1b139169f0baaa8fd2b87c11e9c9b9
YAOrangeTime16 <yokoandrae@gmail.com>
2018-01-28 10:17:47 +0100

    added two tests at PersonaSwitcher

--- src/__tests__/PersonaSwitcher.js
+++ src/__tests__/PersonaSwitcher.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { shallow, render } from 'enzyme';
+import { shallow, render, mount } from 'enzyme';
 import PersonaSwitcher from '../components/PersonaSwitcher';
 
 describe('testing PersonaSwitcher', ()=>{
-    const component = <PersonaSwitcher currentPersona='' changePersona='' />;
-    it('should render the component');
-    it('should hold the value from option tag')
+    const component = <PersonaSwitcher currentPersona='' changePersona={jest.fn()} />;
+    it('renders the component', ()=>{
+        const wrapper = render(component);
+        expect(wrapper.find('div.fixed pin-t pin-r')).toBeTruthy();
+    });
+
+    it('calls onChange and send a value to it', ()=>{
+        const wrapper = shallow(component);
+        const target = {target: {value: 'target value'}};
+        wrapper.find('select').simulate('change', target);
+        expect(wrapper.find('select').props().onChange).toHaveBeenCalledWith(target);
+  
+    });
 });
