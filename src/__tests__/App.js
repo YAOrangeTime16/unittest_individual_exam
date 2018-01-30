381df4d150f5f1bc93a2594660a9e4a038be3258
YAOrangeTime16 <yokoandrae@gmail.com>
2018-01-30 10:16:30 +0100

    settings for deployment

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -11,41 +11,34 @@ describe('testing the App component', ()=>{
     const wrapper = render(component);
     expect(wrapper).toBeDefined();
   });
+  
+  it('toggles the state of currentPage', ()=>{
+    const wrapper = shallow(component);
+    expect(wrapper.state().currentPage).toBe('home');
+    wrapper.find('Button').simulate('click');
+    expect(wrapper.state('currentPage')).toBe('bot');
+    wrapper.find('Button').simulate('click');
+    expect(wrapper.state('currentPage')).toBe('home');
+  });
 
-    /*
-    afterEach(()=>{
-      const wrapper = mount(<App />);
-      wrapper.unmount();
-    });
-    */
-  
-    it('should change state of currentPage', ()=>{
-      const wrapper = shallow(component);
-      expect(wrapper.state().currentPage).toBe('home');
-      wrapper.find('Button').simulate('click');
-      expect(wrapper.state('currentPage')).toBe('bot');
-      wrapper.find('Button').simulate('click');
-      expect(wrapper.state('currentPage')).toBe('home');
-    });
-  
-    it('should show a proper title', ()=>{
-      const wrapper = mount(component);
-      expect(wrapper.find('Button').text()).toBe('Talk to a real human');
-      wrapper.find('Button').simulate('click');
-      expect(wrapper.find('Button').text()).toBe('Return to forum');
-    });
-  
-    it('should render toggle component: Posts/Bot', ()=>{
-      const wrapper = shallow(component);
-      expect(wrapper.containsAllMatchingElements([<Posts currentPersona='' />])).toBeTruthy();
-      wrapper.setState({ currentPage: 'bot' });
-      expect(wrapper.containsAllMatchingElements([<Bot />])).toBeTruthy();
-    });
+  it('shows a proper title', ()=>{
+    const wrapper = mount(component);
+    expect(wrapper.find('Button').text()).toBe('Talk to a real human');
+    wrapper.find('Button').simulate('click');
+    expect(wrapper.find('Button').text()).toBe('Return to forum');
+  });
 
-    it('should change persona', ()=>{
-      const wrapper = shallow(component);
-      const mockEvent = {target: {value: 'alice'}};
-      wrapper.instance().changePersona(mockEvent);
-      expect(wrapper.state('currentPersona')).toBe('alice');
-    });
+  it('renders toggle components: Posts/Bot', ()=>{
+    const wrapper = shallow(component);
+    expect(wrapper.containsAllMatchingElements([<Posts currentPersona='' />])).toBeTruthy();
+    wrapper.setState({ currentPage: 'bot' });
+    expect(wrapper.containsAllMatchingElements([<Bot />])).toBeTruthy();
+  });
+
+  it('should change persona', ()=>{
+    const wrapper = shallow(component);
+    const mockEvent = {target: {value: 'alice'}};
+    wrapper.instance().changePersona(mockEvent);
+    expect(wrapper.state('currentPersona')).toBe(mockEvent.target.value);
+  });
 });
